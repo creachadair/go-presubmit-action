ac35357d1141caa75312e1266d1a88184cc5e81f
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-04-18 14:41:39 -1000

    Fix whitespace.

--- lib.sh
+++ lib.sh
@@ -8,9 +8,9 @@ module() { printf "🛠  \033[1;97m$*\033[0m\n" ; }
 
 check() {
     if [[ $? -ne 0 ]] ; then
-	printf "\033[50C\033[1;31mFAILED\033[0m\n"
-	exit 1
+        printf "\033[50C\033[1;31mFAILED\033[0m\n"
+        exit 1
     else
-	printf "\033[50C\033[1;32mSUCCESS\033[0m\n"
+        printf "\033[50C\033[1;32mSUCCESS\033[0m\n"
     fi
 }
